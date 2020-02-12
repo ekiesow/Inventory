f1ec3b05a77a27e99e8e9aa8f6188bad8e094144
Eric Kiesow <ekiesow@mail.csuchico.edu>
2020-02-12 11:45:59 -0800

    Added summary to Inventory.h

--- Inventory.h
+++ Inventory.h
@@ -5,6 +5,17 @@
 using std::string;
 using std::ostream;
 
+/** 
+ * Class used to keep track of our bakery inventory
+ *
+ * @param m_name      name of item, i.e. Cupcake
+ * @param m_price     price of the item
+ * @param m_in_stock  how much of the item do we have in stock
+ *
+ * Inventory          constructor, args: name, price, amt in stock     
+ * sell               sell the item if it is in stock 
+ * ostream            stream for main to print whether or not item gets sold
+ */
 class Inventory
 {
   private:
@@ -13,9 +24,9 @@ class Inventory
     int m_in_stock;
 
   public:
-    Inventory(string,float,int);
-    void sell();
-    friend ostream& operator<<(ostream&, const Inventory&);
+    Inventory(string,float,int); 
+    void sell(); 
+    friend ostream& operator<<(ostream&, const Inventory&); 
 };
 
 #endif
